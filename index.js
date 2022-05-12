ab288d74d306486a126fdf22c42d402c9a0cc720
godspower <geefeelix@gmail.com>
2022-05-12 14:46:37 -0400

    commit tree 983b9dffe099262c29bd2011de5b00826d3c973d

--- index.js
+++ index.js
@@ -46,3 +46,46 @@ const nestedMuppet = {
 // 7. Use destructuring to assign all variables using the keys as the variable names
 
 // 8. Use destructuring to assign songs 2 and 4, and Kermit's job and partner
+
+const moo = 'cow'
+const neigh = 'horse'
+const baa = 'sheep'
+const oink = 'pig'
+const cluck = 'chicken'
+
+const bessie = 'cow'
+const dolly = 'sheep'
+const babe = 'pig'
+const little = 'chicken'
+
+const blackAndWhite = 'cow'
+const black = 'sheep'
+const pink = 'pig'
+
+const red = 'red'
+const orange = 'orange'
+const yellow = 'yellow'
+const green = 'green'
+const blue = 'blue'
+const indigo = 'indigo'
+const violet = 'violet'
+
+const r = 'red'
+const o = 'orange'
+const y = 'yellow'
+const g = 'green'
+const b = 'blue'
+const v = 'violet'
+
+const indg = 'indigo'
+
+const muppetName = 'Miss Piggy'
+const color = 'pink'
+const song = 'Never Before, Never Again'
+const job = 'Cast member of The Muppet Show'
+const partner = 'Kermit'
+
+const song2 = 'Moving Right Along'
+const song4 = 'I Hope That Something Better Comes Along'
+const nestedJob = 'Host of The Muppet Show'
+const nestedPartner = 'Miss Piggy'
